38e76d5f07ccd6093a521a0cc0dc035707ea4deb
unknown <antoniofontanilla9@gmail.com>
2026-09-23 09:57:51 -0300

    Desactivar ssl_require para corregir error de conexion MySQL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=False  # Cambiado a False para evitar conflictos con PyMySQL en Clever Cloud
     )
 }
 
